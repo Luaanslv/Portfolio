8d94e3f0a9feb8613a7786c8a4be6a02c415f410
Luaanslv <luaan.silva117@gmail.com>
2021-12-01 16:05:58 -0300

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", function () {
+    var sidebar = document.querySelector(".container").classList.toggle("show-menu")
+});
+
+
+
+
