fd486839157e3baf05faeeb2bb79a419ae18c162
Luaanslv <luaan.silva117@gmail.com>
2021-12-02 13:32:07 -0300

    :dragon: script esconde menu ao ser clicado

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,11 @@ hamburguer.addEventListener("click", function () {
     var sidebar = document.querySelector(".container").classList.toggle("show-menu")
 });
 
+const menu = document.querySelector(".menu");
+
+menu.addEventListener("click", function () {
+     var sidebar = document.querySelector(".container").classList.toggle("show-menu")
+})
 
 
 
