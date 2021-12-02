be1886d184a8b7a9c827110a74f789e34a48ea72
Luaanslv <luaan.silva117@gmail.com>
2021-12-02 13:32:22 -0300

    ...

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,11 @@
+//manu aparece 
 var hamburguer = document.querySelector(".hamburguer");
 
 hamburguer.addEventListener("click", function () {
     var sidebar = document.querySelector(".container").classList.toggle("show-menu")
 });
 
+//menu esconde
 const menu = document.querySelector(".menu");
 
 menu.addEventListener("click", function () {
