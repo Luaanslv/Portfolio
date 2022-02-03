ade0cda33c187882bc8efc6b0ed218aba855f232
Luaanslv <luaan.silva117@gmail.com>
2022-02-03 16:38:36 -0300

    em progresso

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,14 @@
 
+var menu = document.getElementById('menu');
+var numPx = '300';
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY > numPx) {
+        menu.classList.remove('onTop');
+    } else {
+        menu.classList.add('onTop');
+    }
+});
+
 
 
