019b895ce53b2f19a71d43505887b88f2b57cfb3
Luaanslv <luaan.silva117@gmail.com>
2022-02-04 11:58:11 -0300

    Atualiazação

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,3 @@
 
-var menu = document.getElementById('menu');
-var numPx = '300';
-
-window.addEventListener('scroll', function () {
-    if (window.scrollY > numPx) {
-        menu.classList.remove('onTop');
-    } else {
-        menu.classList.add('onTop');
-    }
-});
-
 
 
