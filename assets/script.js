d5f04d96e4f0367eb05a3977114fe05062ac366d
Luaanslv <luaan.silva117@gmail.com>
2022-02-04 16:38:27 -0300

    Animaçao

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,30 @@
+const debounce = function (func, wait, immediate) {
+    let timeout;
+    return function (...args) {
+        const context = this;
+        const later = function () {
+            timeout = null;
+        }
+    }
+}
 
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
 
+function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) /4);
+    target.forEach(function (element) {
+        if ((windowTop) > element.offsetTop) {
+            element.classList.add(animationClass);
+        } else {
+        element.classList.remove(animationClass);
+    }
+    }) 
+}
+animeScroll();
+if (target.length) {
+    window.addEventListener('scroll', function () {
+    animeScroll();
+})
+}
 
