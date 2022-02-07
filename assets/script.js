c6cef8f274649f648630845e4fb79dea6751a2c2
Luaanslv <luaan.silva117@gmail.com>
2022-02-07 11:43:30 -0300

    menu mobile finalizado

--- assets/script.js
+++ assets/script.js
@@ -28,11 +28,11 @@ if (target.length) {
 })
 }
 // script menu
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+const btnMobile = document.querySelector('#btn-Mobile');
+
+function toggleMenu() {
+    const nav = document.querySelector('#nav');
+    nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
