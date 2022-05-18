d01255d965ec4956b2b1d41ebdd8d1f58e4c1479
Luaanslv <luaan.silva117@gmail.com>
2022-05-18 17:26:03 -0300

    sobre e skills

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
 var btnDiv = document.querySelector('#show');
 var div = document.querySelector('#descricao');
 var container = document.querySelector('.container')
-var btnExit = document.querySelector('#exit');
-
+var btnExit = document.querySelector('#exit-sobre');
+var divSKills = document.querySelector('#skills');
+var btnUrano = document.querySelector('#urano');
+var btnExitSkills = document.querySelector('#exitSkills')
 
 btnDiv.addEventListener('click', function () {
-    
-    container.style.backgroundColor = 'rgba(0, 0, 0, 0.658)';
     
     if (div.style.display === "block") {
         div.style.display = "none";
@@ -19,5 +19,20 @@ btnDiv.addEventListener('click', function () {
 btnExit.addEventListener('click', function () {
     if (div.style.display === "block") {
         div.style.display = "none"
+    }
+})
+
+btnUrano.addEventListener('click', function () {
+
+    if (divSKills.style.opacity === "block") {
+        divSKills.style.display = "none";
+    } else {
+        divSKills.style.display = "block";
+    }
+})
+
+btnExitSkills.addEventListener('click', function () {
+    if (divSKills.style.display === "block") {
+        divSKills.style.display = "none"
     }
 })
