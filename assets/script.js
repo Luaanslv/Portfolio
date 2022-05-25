61af8d6cb0b7e29b54a9b652fa7baf731f0be5c9
Luaanslv <luaan.silva117@gmail.com>
2022-05-25 10:44:41 -0300

    feat: imagens e styles

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,21 @@
 var btnDiv = document.querySelector('#show');
 var div = document.querySelector('#descricao');
-var container = document.querySelector('.container')
 var btnExit = document.querySelector('#exit-sobre');
 var divSKills = document.querySelector('#skills');
 var btnUrano = document.querySelector('#urano');
-var btnExitSkills = document.querySelector('#exitSkills')
+var btnExitSkills = document.querySelector('#exitSkills');
+var btnPlanet = document.querySelector('#btnPlanet');
+var btnExitProjects = document.querySelector('#exitProjects');
+var divProjects = document.querySelector('#divProjects');
 
 btnDiv.addEventListener('click', function () {
-    
-    if (div.style.display === "block") {
-        div.style.display = "none";
+
+    if (div.style.display === "none") {
+        div.style.display = "block";
     } else {
         div.style.display = "block";
     }
-    
+
 })
 
 btnExit.addEventListener('click', function () {
@@ -24,8 +26,8 @@ btnExit.addEventListener('click', function () {
 
 btnUrano.addEventListener('click', function () {
 
-    if (divSKills.style.opacity === "block") {
-        divSKills.style.display = "none";
+    if (divSKills.style.display === "none") {
+        divSKills.style.display = "block";
     } else {
         divSKills.style.display = "block";
     }
@@ -34,5 +36,20 @@ btnUrano.addEventListener('click', function () {
 btnExitSkills.addEventListener('click', function () {
     if (divSKills.style.display === "block") {
         divSKills.style.display = "none"
+    }
+})
+
+btnPlanet.addEventListener('click', function () {
+
+    if (divProjects.style.display === "none") {
+        divProjects.style.display = "block";
+    } else {
+        divProjects.style.display = "block";
+    }
+
+})
+btnExitProjects.addEventListener('click', function () {
+    if (divProjects.style.display === "block") {
+        divProjects.style.display = "none";
     }
 })
